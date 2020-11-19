77fb37f91d6f40c0dc0287308c59eef5a80c6889
AbsoluteVirtueXI <absolutevirtuexi@gmail.com>
2020-11-19 16:49:31 +0100

    test ok

--- migrations/2_deploy_GameLoot.js
+++ migrations/2_deploy_GameLoot.js
@@ -1,5 +1,8 @@
 const GameLoot = artifacts.require('GameLoot');
 
+// Attention si bug PoolError il faut modifier:
+// node_modules/@trufflesuite/web3-provider-engine/subproviders/rpc.js line 34 et set
+// le timeout à minimum 100000 (par défaut c'est 20000)
 module.exports = async function (deployer, network, accounts) {
   await deployer.deploy(GameLoot, accounts[0], { from: accounts[0] });
 };
--- test/GameLoot_test.js
+++ test/GameLoot_test.js
@@ -1,18 +1,30 @@
+/* eslint-disable comma-dangle */
 /* eslint-disable no-unused-expressions */
 const { contract, accounts } = require('@openzeppelin/test-environment');
 const { BN, singletons, expectRevert } = require('@openzeppelin/test-helpers');
+const { web3 } = require('@openzeppelin/test-helpers/src/setup');
 const { expect } = require('chai');
 
 const GameLoot = contract.fromArtifact('GameLoot');
+const isSameLoot = (_loot1, loot1) => {
+  return (
+    _loot1 === loot1[0] &&
+    new BN(_loot1[1]).eq(loot1[1]) &&
+    new BN(_loot1[2]).eq(loot1[2]) &&
+    new BN(_loot1[3]).eq(loot1[3])
+  );
+};
 
 describe('GameLoot', function () {
   const [owner, dev, user1, user2] = accounts;
   const NAME = 'GameLoot';
   const SYMBOL = 'LOOT';
-  const loot1 = { name: 'Mercurial krys', lootType: new BN(0), attk: new BN(15), def: new BN(0) };
-  const loot2 = { name: 'Aegis', lootType: new BN(1), attk: new BN(0), def: new BN(15) };
-  const loot3 = { name: 'Excalibur', lootType: new BN(0), attk: new BN(30), def: new BN(5) };
-  const loot4 = { name: 'Helm of darkness', lootType: new BN(1), attk: new BN(4), def: new BN(45) };
+  // A VERIFIER SI UPDATE POUR STRUCT
+  const loot1 = ['Mercurial krys', new BN(0), new BN(15), new BN(0)];
+  const loot2 = ['Aegis', new BN(1), new BN(0), new BN(15)];
+  const loot3 = ['Excalibur', new BN(0), new BN(30), new BN(5)];
+  const loot4 = ['Helm of darkness', new BN(1), new BN(4), new BN(45)];
+
   beforeEach(async function () {
     this.gameloot = await GameLoot.new(owner, { from: dev });
   });
@@ -28,10 +40,27 @@ describe('GameLoot', function () {
   it('reverts if loot() not called by owner', async function () {
     await expectRevert(this.gameloot.loot(user1, loot1, { from: dev }), 'Ownable: caller is not the owner');
   });
+
   it('increments tokenId by calling loot()', async function () {
     await this.gameloot.loot(user1, loot1, { from: owner });
-    await this.gameloot.loot(user2, loot2, { from: owner });
+    expect(await this.gameloot.tokenOfOwnerByIndex(user1, new BN(0)), 'id should be 1').to.be.bignumber.equal(
+      new BN(1)
+    );
+    await this.gameloot.loot(user1, loot2, { from: owner });
+    expect(await this.gameloot.tokenOfOwnerByIndex(user1, new BN(1)), 'id should be 2').to.be.bignumber.equal(
+      new BN(2)
+    );
   });
+
+  it('set tokenId to loot', async function () {
+    await this.gameloot.loot(user1, loot1, { from: owner });
+    await this.gameloot.loot(user1, loot2, { from: owner });
+    const _loot1 = await this.gameloot.getLootById(new BN(1));
+    const _loot2 = await this.gameloot.getLootById(new BN(2));
+    expect(isSameLoot(_loot1, loot1)).to.be.true;
+    expect(isSameLoot(_loot2, loot2)).to.be.true;
+  });
+
   it('mints NFT to user by calling loot()', async function () {
     await this.gameloot.loot(user1, loot1, { from: owner });
     await this.gameloot.loot(user2, loot2, { from: owner });
